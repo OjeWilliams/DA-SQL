95be44729f55ce3206dc9fc0a686d42e918ec6c7
Oje Williams <jaydozrock@gmail.com>
2021-09-28 19:29:49 -0400

    initial commit

---
+++ codes/Problems.md
@@ -0,0 +1,18 @@
+## All questions can be found [here](https://quip.com/2gwZArKuWk7W)
+
+Self-Join Practice Problems 
+
+#1: MoM Percent Change 
+
+*Context:* Oftentimes it's useful to know how much a key metric, such as monthly active users, changes between months. Say we have a table logins in the form: 
+| user_id | date       |
+|---------|------------|
+| 1       | 2018-07-01 |
+| 234     | 2018-07-02 |
+| 3       | 2018-07-02 |
+| 1       | 2018-07-02 |
+| ...     | ...        |
+| 234     | 2018-10-04 |
+
+**Task**: Find the month-over-month percentage change for monthly active users (MAU). 
+
---
+++ codes/mysq.sql
@@ -0,0 +1,37 @@
+
+# Solution 1
+
+WITH MoM as
+(
+    SELECT EXTRACT(month FROM starttime) AS mymonth,
+    COUNT(DISTINCT memid) AS usr
+    FROM cd.bookings
+    GROUP BY mymonth 
+)
+
+SELECT 
+a.mymonth AS old_month, a.usr AS old_usrc,
+b.mymonth AS new_month, b.usr AS new_usrc,
+ROUND((100*(b.usr - a.usr) / a.usr),2) as perc_change
+FROM MoM AS a 
+INNER JOIN MoM AS b on a.mymonth = b.mymonth - 1;
+
+###########################################################
+###########################################################
+
+# Solution 2
+
+WITH MoM as
+(
+    SELECT DATE_TRUNC('month', starttime) AS mymonth,
+    COUNT(DISTINCT memid) AS usr_count
+    FROM cd.bookings
+    GROUP BY mymonth 
+)
+
+SELECT 
+A.mymonth AS old_month, A.usr_count AS old_usrc,
+B.mymonth AS new_month, B.usr_count AS new_usrc,
+ROUND(100*(B.usr_count - A.usr_count) / A.usr_count,2) as perc_change
+FROM MoM AS A 
+INNER JOIN MoM AS B on A.mymonth = B.mymonth - INTERVAL '1 month' ;
