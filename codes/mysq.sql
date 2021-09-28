ac274f4a16c7f93516b7d698932ab8ddf0d1707a
Oje Williams <jaydozrock@gmail.com>
2021-09-28 19:33:27 -0400

    minor fixes

--- codes/mysq.sql
+++ codes/mysq.sql
@@ -1,7 +1,8 @@
 
-# Solution 1
+# Q1
+## Solution 1
 
-WITH MoM as
+WITH MoM AS
 (
     SELECT EXTRACT(month FROM starttime) AS mymonth,
     COUNT(DISTINCT memid) AS usr
@@ -16,12 +17,9 @@ ROUND((100*(b.usr - a.usr) / a.usr),2) as perc_change
 FROM MoM AS a 
 INNER JOIN MoM AS b on a.mymonth = b.mymonth - 1;
 
-###########################################################
-###########################################################
+## Solution 2
 
-# Solution 2
-
-WITH MoM as
+WITH MoM AS
 (
     SELECT DATE_TRUNC('month', starttime) AS mymonth,
     COUNT(DISTINCT memid) AS usr_count
